d57dcb61e61f353e3d74c4a55e06c5c0e41cf715
odon-airoldi <odon.airoldi@gmail.com>
2026-02-25 12:04:41 +0100

    forEach loop output card photo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+
+const row_el = document.querySelector('.row');
+
 fetch(endpoint)
     .then(res => res.json())
     .then(data => {
 
 
-        console.log(data)
+
 
         data.forEach(photo => {
             const {title, date, url} = photo
             console.log(title, date, url);
+
+            const markup = `<div class="card-photo bg-white px-3 pt-3 pb-4 position-relative">
+                    <figure class="mb-0">
+                        <img class="mb-3" src="${url}" alt="">
+                        <figcaption><p>${title}</p></figcaption>
+                    </figure> 
+                </div>`
+
+            row_el.insertAdjacentHTML('beforeend', markup)
+            
         })
+
+
 
     })
