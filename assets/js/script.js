dcf655b0b3cb317f454a9c1a096b06c1fc372dcf
odon-airoldi <odon.airoldi@gmail.com>
2026-02-25 12:15:27 +0100

    css adjustment

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,14 @@ fetch(endpoint)
             const {title, date, url} = photo
             console.log(title, date, url);
 
-            const markup = `<div class="card-photo bg-white px-3 pt-3 pb-4 position-relative">
-                    <figure class="mb-0">
-                        <img class="mb-3" src="${url}" alt="">
-                        <figcaption><p>${title}</p></figcaption>
-                    </figure> 
-                </div>`
+            const markup = `<div>
+                                <div class="card-photo bg-white px-3 pt-3 pb-4 position-relative">
+                                    <figure class="mb-0">
+                                        <img class="mb-3" src="${url}" alt="">
+                                        <figcaption><p>${title}</p></figcaption>
+                                    </figure> 
+                                </div>
+                            </div>`
 
             row_el.insertAdjacentHTML('beforeend', markup)
             
