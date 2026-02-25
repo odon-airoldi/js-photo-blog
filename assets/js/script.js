64820a17d2d91a6ccfdbfb9cff55ddcf543c222c
odon-airoldi <odon.airoldi@gmail.com>
2026-02-25 10:27:04 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/int'
+const resultEl = document.getElementById('result')
+
+fetch(endpoint)
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data)
+        resultEl.innerHTML = data.response
+
+    })
+*/
