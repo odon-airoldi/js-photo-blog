f05a098f8e2b359483095e452182334dba3c4329
odon-airoldi <odon.airoldi@gmail.com>
2026-02-25 12:36:13 +0100

    end milestone 2 - 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,30 @@
 const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
-
 const row_el = document.querySelector('.row');
+
 
 fetch(endpoint)
     .then(res => res.json())
     .then(data => {
-
-
-
 
         data.forEach(photo => {
             const {id, title, date, url} = photo
             console.log(id, title, date, url);
 
             const markup = `<div>
-                                <div id="card-photo-${id}" class="card-photo bg-white px-3 pt-3 pb-4 position-relative">
+                                <div id="card-photo-${id}" class="card-photo bg-white p-3 position-relative">
                                     <figure class="mb-0">
                                         <img class="mb-3" src="${url}" alt="${title}">
                                         <figcaption>
                                             <small class="photo-date text-secondary">${date}</small>
-                                            <p class="photo-title mb-0">${title}</p>
+                                            <p class="photo-title mb-0 fs-4 text-uppercase fw-bold lh-sm">${title}</p>
                                         </figcaption>
                                     </figure> 
                                 </div>
                             </div>`
 
             row_el.insertAdjacentHTML('beforeend', markup)
+
             
         })
-
-
 
     })
