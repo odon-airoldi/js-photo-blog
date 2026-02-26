477a6d62bf2cb26281da3cbb7285129ea7707ea5
odon-airoldi <odon.airoldi@gmail.com>
2026-02-26 11:46:47 +0100

    add js open overlay

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,21 @@ const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
 const row_el = document.querySelector('.row');
 
 
-fetch(endpoint)
+
+
+    fetch(endpoint)
     .then(res => res.json())
     .then(data => {
 
         data.forEach(photo => {
             const {id, title, date, url} = photo
 
-            const markup = `<div id="card-photo-${id}">
-                                <div class="card-photo bg-white p-3 position-relative">
+            const col = document.createElement('div');
+            col.id = `card-photo-${id}`
+            row_el.appendChild(col)
+            
+            const markup = `<div class="card-photo position-relative">
+                                <div class="card-content bg-white p-3">
                                     <figure class="mb-0">
                                         <img class="mb-3" src="${url}" alt="${title}">
                                         <figcaption>
@@ -21,50 +27,65 @@ fetch(endpoint)
                                 </div>
                             </div>`
 
-            row_el.insertAdjacentHTML('beforeend', markup)
+            col.innerHTML = markup
 
         })
+
+        myFunction();
 
     })
 
 
 
 const overlay_el = document.querySelector('.overlay');
+const close_el = document.querySelector('.close');
 
-overlay_el.addEventListener('click', function(){
 
-    this.classList.add('d-none')
+close_el.addEventListener('click', function(){
+
+    overlay_el.classList.add('d-none')
 
 });
 
+function myFunction() {
 
-    /*
+    const card_el = document.getElementById('card-photo-1')
+    console.log(card_el)
 
-    fetch(endpoint)
-    .then(res => res.json())
-    .then(data => {
+    card_el.addEventListener('click', function(){
 
-        data.forEach(photo => {
-            const {id, title, date, url} = photo
+        overlay_el.classList.remove('d-none')
 
-            const col = document.createElement('div');
-            col.classList.add(`card-photo-${id}`)
-            row_el.appendChild(col)
-            
-            const markup = `<div class="card-photo bg-white p-3 position-relative">
-                                    <figure class="mb-0">
-                                        <img class="mb-3" src="${url}" alt="${title}">
-                                        <figcaption>
-                                            <small class="photo-date text-secondary">${date}</small>
-                                            <p class="photo-title mb-0 fs-4 text-uppercase fw-bold lh-sm">${title}</p>
-                                        </figcaption>
-                                    </figure> 
-                                </div>`
+    });
 
-            col.innerHTML = markup
+}
 
-        })
 
-    })
 
-    */
+ 
+
+
+// fetch(endpoint)
+//     .then(res => res.json())
+//     .then(data => {
+
+//         data.forEach(photo => {
+//             const {id, title, date, url} = photo
+
+//             const markup = `<div id="card-photo-${id}" class="card-photo position-relative">
+//                                 <div class="card-content bg-white p-3">
+//                                     <figure class="mb-0">
+//                                         <img class="mb-3" src="${url}" alt="${title}">
+//                                         <figcaption>
+//                                             <small class="photo-date text-secondary">${date}</small>
+//                                             <p class="photo-title mb-0 fs-4 text-uppercase fw-bold lh-sm">${title}</p>
+//                                         </figcaption>
+//                                     </figure> 
+//                                 </div>
+//                             </div>`
+
+//             row_el.insertAdjacentHTML('beforeend', markup)
+
+//         })
+
+//     })
